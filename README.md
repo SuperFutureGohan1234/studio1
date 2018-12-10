2d459cfe6fd69043d135a58f1146fa0b40a683fa
Adrian Herbert <SuperFutureGohan1234@github.com>
2018-12-10 17:58:19 -0500

    I did things.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # studio1
+Our chatroom is comprised of author names, timestamps and strings for the messages.
--- js/index.js
+++ js/index.js
@@ -1,6 +1,60 @@
 // Write your JS here
+
 import test from './script2.js';
+
 
 console.log('test');
 
+
 test();
+
+
+
+//Set database object
+
+var database = firebase.database().ref();
+
+    
+
+//button executes this function
+
+function updateDB(){
+
+     var name = $("#name").val();
+
+     var message = $("#message").val();
+
+     console.log(name + " : " + message);
+
+
+     //Update database here
+
+     var value = {name : name, message : message};
+
+     database.push(value);
+
+}
+
+
+
+$("#submit").click(updateDB);
+
+
+
+database.on("child_added",function(rowData){
+
+
+
+     console.log(rowData.val());
+
+     var name = rowData.val().name;
+
+     var message = rowData.val().message;
+
+     var html = "<p>" + name + ":" + message + "</p>";
+
+     $(".allMessages").append(html);
+
+
+
+});
